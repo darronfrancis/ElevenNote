de315f13940b103c9ef8ddad6eadf8cd0575ba58
Darron <darronfrancis@gmail.com>
2020-10-20 20:55:39 -0400

    added noteController

--- ElevenNote.Models/NoteCreate.cs
+++ ElevenNote.Models/NoteCreate.cs
@@ -7,10 +7,13 @@ using System.Threading.Tasks;
 
 namespace ElevenNote.Models
 {
-    class NoteCreate
+    public class NoteCreate
     {
         [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
         public string Title { get; set; }
+        [MaxLength(8000)]
         public string Content { get; set; }
     }
 }
