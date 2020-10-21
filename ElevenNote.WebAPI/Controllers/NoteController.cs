ec66ead3c8eef2c46b63dd3b98defa72db32c5b4
Darron <darronfrancis@gmail.com>
2020-10-20 21:08:40 -0400

    added getNoteByID endpoint

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -14,34 +14,29 @@ namespace ElevenNote.WebAPI.Controllers
     [Authorize]
     public class NoteController : ApiController
     {
+        public IHttpActionResult Get()
+        {
+            NoteService noteService = CreateNoteService();
+            var notes = noteService.GetNotes();
+            return Ok(notes);
+        }
+        public IHttpActionResult Post(NoteCreate note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteService();
+
+            if (!service.CreateNote(note))
+                return InternalServerError();
+
+            return Ok();
+        }
         private NoteService CreateNoteService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
             var noteService = new NoteService(userId);
             return noteService;
         }
-    }
-    public IHttpActionResult Get()
-    {
-        NoteService noteService = CreateNoteService();
-        var notes = noteService.GetNotes();
-        return Ok(notes);
-    }
-    public IHttpActionResult Post(NoteCreate note)
-    {
-        if (!ModelState.IsValid)
-            return BadRequest(ModelState);
-
-        var service = CreateNoteService();
-
-        if (!service.CreateNote(note))
-            return InternalServerError();
-
-        return Ok();
-    }
-
-    object CreateNoteService()
-    {
-        throw new NotImplementedException();
     }
 }
